722de55f6dac599cdfd6a270b32e218ce9c635ed
rhicjospeh04 <rhicchavez04@gmail.com>
2023-09-01 02:47:46 +0800

    addfiles

--- src/index.js
+++ src/index.js
@@ -15,3 +15,20 @@ kilogramsToGrams.addEventListener('click', () => {
        
     }
 });
+
+const celsiusToFahrenheit = document.getElementById('celsiusToFahrenheit');
+
+celsiusToFahrenheit.addEventListener('click', () => {
+    let num1 = document.getElementById('num1');
+    let num2 = document.getElementById('num2');
+    
+    
+ if (num2.value === '') {
+        num2.value = parseFloat(num1.value * 9/5) + 32;
+     
+    } else if (num1.value === '') {
+       num1.value = (parseFloat(num2.value - 32) / 9/5).toFixed(2);
+       
+    }
+});
+
